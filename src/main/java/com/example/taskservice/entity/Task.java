d97947a3ffaf4993bc914c001ecef794f398af3b
abrarriyad <rashidabrarriyad@gmail.com>
2025-06-06 05:14:14 +0600

    fix: null value in response

--- src/main/java/com/example/taskservice/entity/Task.java
+++ src/main/java/com/example/taskservice/entity/Task.java
@@ -1,10 +1,11 @@
 package com.example.taskservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Task {
-
+    @JsonProperty("Id")
     private String id;
     private String description;
 
